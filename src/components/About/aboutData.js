46bbc3b923b5196eb5a9d915362d9494d7e6f97b
tomaszkarny <45500472+tomaszkarny@users.noreply.github.com>
2024-01-04 21:07:54 +0100

    Update aboutData.js

--- src/components/About/aboutData.js
+++ src/components/About/aboutData.js
@@ -2,7 +2,7 @@ import people from 'assets/people.jpg';
 
 export const aboutData = {
   title: 'About Me',
-  text: `My name is Tomasz. I'am interested in web development. Every day I'am learning new things and broaden my knowlege.  My advantages are conscientiousness and perseverance in performed activities. Currently I'am working  in manufacturing industry as a cnc operator - programmer. I'am living in Białystok. I would like to become a web developer.`,
+  text: `Hi, I'm Tomasz. My journey in web development is driven by a meticulous attention to detail, honed through years as a CNC programmer. There, I learned the power of precision and structured problem-solving—skills that now fuel my approach to crafting seamless web experiences. As a freelance developer, I'm eager to bring that same rigor and innovation to your digital projects. Let's connect and create something impactful.`,
 
   image: people,
   alt: 'people',
